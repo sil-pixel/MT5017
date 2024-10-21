d1a34a567f947129ffd8226e33dfc54bedabc7a2
sil-pixel <silpa2408@gmail.com>
2024-10-21 18:51:02 +0200

    loocv loop fix

--- CA3/codes_assignment_3.R
+++ CA3/codes_assignment_3.R
@@ -23,29 +23,33 @@ n <- nrow(X)
 theta0 = rep(0, ncol(X))
 k <- length(theta0)
 theta_estimate <- NR(theta0, 3, y, X) 
-log_likellihood <- l(theta_estimate, y, X)
-aic_computed <- 2*k - 2*log_likellihood
+log_likelihood <- l(theta_estimate, y, X)
+aic_computed <- 2*k - 2*log_likelihood
 
 #AIC output from R summary 
 r_summary_aic <- summary(modell)$aic
 
-# Compute n*k_cv = l(theta_ml) + sum((S_i(theta_i))T*(theta_i - theta_ml))
+# Compute k_cv =sum(l_i(theta_i))/n
 # Here, theta_i = theta_ml(X_-i)
-nk_cv <- log_likellihood
+nk_cv <- 0
 for(i in 1:n) {
   X_minus_i <- X[-i, , drop=FALSE]
   y_minus_i <- y[-i, , drop=FALSE]
+  X_i <- X[i, , drop=FALSE]
+  y_i <- y[i, , drop=FALSE]
   theta_i <- NR(theta0, 3, y_minus_i, X_minus_i)
-  score <- S(theta_i, y_minus_i, X_minus_i)
-  subset_sum <- t(score) %*% (theta_i - theta_estimate)
-  nk_cv <- nk_cv + subset_sum
+  # log likelihood for i-th observation
+  log_likelihood_theta_i <- l(theta_i, y_i, X_i)
+  nk_cv <- nk_cv + log_likelihood_theta_i
 }
+
+k_cv <- nk_cv/n
 
 # Creating a comparison data frame 
 comparison_aic_values <- data.frame(
   "AIC_R_model" = r_summary_aic,
   "AIC_computed" = aic_computed,
-  "NKCV_computed" = -2*nk_cv
+  "2*nK_CV_computed" = 2*nk_cv
 )
 
 # ---- Task_2 ----
